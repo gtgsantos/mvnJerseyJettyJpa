eda37e1207fbc32d7e3d27046e16427c63967a55
Guilherme Santos <guilherme.santos@gmail.com>
2017-12-05 00:09:59 -0300

    adding maven dependencies

--- src/main/java/com/gtgsantos/main/Test.java
+++ src/main/java/com/gtgsantos/main/Test.java
@@ -4,7 +4,7 @@ import javax.persistence.Persistence;
 
 public class Test {
     public static void main(String[] args) {
-        Persistence.createEntityManagerFactory("AlgaWorksPU");
+        Persistence.createEntityManagerFactory("entityManagerDevAmbient");
     }
 }
 
